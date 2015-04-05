3ae571244444bc23e63745d7c96f5bd6254ca036
Sebastian Olsson <sebelino@hotmail.com>
2015-04-05 22:53:17 +0200

    Created a signal class and a segmentation method.

---
+++ Signal.m
@@ -0,0 +1,27 @@
+classdef Signal
+    properties
+        Graph
+        Unit
+    end
+    methods
+        function self = Signal(time,unit,recording)
+            cellarray = num2cell([time,recording]);
+            self.Graph = cell2struct(cellarray,{'Time','Quantity'},2);
+            self.Unit = unit;
+        end
+        function segments=segment(self,len)
+            m = mod([self.Graph.Time]',len);
+            timeintervals = accumarray(cumsum([0;diff(m(:))] < 0)+1,m,[],@(x) {x});
+            segmentlengths = cellfun(@length,timeintervals);
+            segmentindices = cumsum(segmentlengths)-segmentlengths+1;
+            quantities = [self.Graph.Quantity]';
+            for i=1:size(segmentlengths,1)
+                segmentquantities(i) = {quantities(segmentindices(i):segmentindices(i)+segmentlengths(i)-1)};
+            end
+            segmentquantities = segmentquantities';
+            zip = num2cell([timeintervals,segmentquantities],2);
+            segments = arrayfun(@(p){Signal(p{1}{1},self.Unit,p{1}{2})},zip);
+            segments = [segments{:}]';
+        end
+    end
+end
--- hypnoscorer.m
+++ hypnoscorer.m
@@ -4,5 +4,8 @@ addpath('lib/wfdb-toolbox/mcode/')
 recordpath = 'data/slp01a/slp01a';
 [tm,signal,Fs,siginfo] = rdmat(strcat(recordpath,'m'));
 physicaleeg = signal(:,3);
-[ann,type,subtype,chan,num,comments] = rdann(recordpath,'st');
 
+eeg = Signal(tm',siginfo(3).Units,physicaleeg)
+
+%[ann,type,subtype,chan,num,comments] = rdann(recordpath,'st');
+%annotations = [char([comments{:}]),num2str(ann)];
