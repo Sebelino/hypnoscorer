b6dd9de30b78b6ac0f95fb7b7b741f75e0728538
Sebastian Olsson <sebelino@hotmail.com>
2015-04-09 23:51:41 +0200

    Added new features: variance, standard deviation, skewness, kurtosis.

--- Featurevector.m
+++ Featurevector.m
@@ -3,12 +3,21 @@ classdef Featurevector
     % a signal segment.
 
     properties(SetAccess='private')
-        Mean % The mean of the physical quantity of the signal segment
+        Mean % Mean of the physical quantity of the signal segment
+        Variance % Variance of the physical quantity of the signal segment
+        StandardDeviation % Standard deviation of the physical quantity of the signal segment
+        Skewness % Skewness of the physical quantity of the signal segment
+        Kurtosis % Kurtosis of the physical quantity of the signal segment
     end
     methods
         function self = Featurevector(featureStruct)
             self.Mean = featureStruct.Mean;
+            self.Variance = featureStruct.Variance;
+            self.StandardDeviation = featureStruct.StandardDeviation;
+            self.Skewness = featureStruct.Skewness;
+            self.Kurtosis = featureStruct.Kurtosis;
             % TODO for p in properties(fs): self.p = fs.p
+            %features = fieldnames(self);
         end
     end
 end
