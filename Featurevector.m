a62e257f8c1eea755f34f9227e302279baa54d6c
Sebastian Olsson <sebelino@hotmail.com>
2015-04-10 15:38:52 +0200

    Reimplemented Featurevector as to contain a struct to allow for vectors of different dimensions.

--- Featurevector.m
+++ Featurevector.m
@@ -1,24 +1,33 @@
-classdef Featurevector
+classdef Featurevector < matlab.mixin.CustomDisplay
     % Featurevector A vector of features represented numerically, extracted from
     % a signal segment.
 
-    properties(SetAccess='private')
-        Mean % Mean of the physical quantity of the signal segment
-        Variance % Variance of the physical quantity of the signal segment
-        StandardDeviation % Standard deviation of the physical quantity of the signal segment
-        Skewness % Skewness of the physical quantity of the signal segment
-        Kurtosis % Kurtosis of the physical quantity of the signal segment
-        %Mode % Mode of the physical quantity of the signal segment
+    properties(SetAccess=private)
+        Vector % Struct mapping a feature (e.g. "Mean") to a numerical value (e.g. -0.0222)
     end
     methods
         function self = Featurevector(featureStruct)
-            features = fieldnames(self);
-            for i = 1:self.dimension
-                self.(features{i}) = featureStruct.(features{i});
-            end
+            self.Vector = featureStruct;
         end
         function count = dimension(self)
             count = numel(fieldnames(self));
+        end
+    end
+    methods(Access=protected)
+        function displayScalarObject(self)
+            className = matlab.mixin.CustomDisplay.getClassNameForHeader(self);
+            scalarHeader = [className,' with properties:'];
+            header = sprintf('%s\n',scalarHeader);
+            disp(header)
+            propgroup = getPropertyGroups(self);
+            matlab.mixin.CustomDisplay.displayPropertyGroups(self,propgroup);
+        end
+        function pg = getPropertyGroups(self)
+            if ~isscalar(self)
+                pg = getPropertyGroups@matlab.mixin.CustomDisplay(self);
+            else
+                pg(1) = matlab.mixin.util.PropertyGroup(self.Vector,'Vector');
+            end
         end
     end
 end
