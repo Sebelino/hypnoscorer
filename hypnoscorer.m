d1fed9f347cd0618def56045e2d7ae4d152e0508
Sebastian Olsson <sebelino@hotmail.com>
2015-04-10 15:39:21 +0200

    Minor.

--- hypnoscorer.m
+++ hypnoscorer.m
@@ -6,6 +6,9 @@ recordpath = 'data/slp01a/slp01a';
 physicaleeg = signal(:,3);
 
 eeg = Signal(tm',siginfo(3).Units,physicaleeg)
+ss = eeg.segment(1)
+s = ss(1)
+f = s.features
 
 %[ann,type,subtype,chan,num,comments] = rdann(recordpath,'st');
 %annotations = [char([comments{:}]),num2str(ann)];
