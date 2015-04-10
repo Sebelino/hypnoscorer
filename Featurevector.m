b0f35f609fc565270ef6012933a3625c9cae3cba
Sebastian Olsson <sebelino@hotmail.com>
2015-04-10 17:10:59 +0200

    Documented Featurevector.

--- Featurevector.m
+++ Featurevector.m
@@ -10,11 +10,13 @@ classdef Featurevector < matlab.mixin.CustomDisplay
             self.Vector = featureStruct;
         end
         function count = dimension(self)
+            % The number of features
             count = numel(fieldnames(self));
         end
     end
     methods(Access=protected)
         function displayScalarObject(self)
+            % Cosmetic
             className = matlab.mixin.CustomDisplay.getClassNameForHeader(self);
             scalarHeader = [className,' with properties:'];
             header = sprintf('%s\n',scalarHeader);
@@ -23,11 +25,15 @@ classdef Featurevector < matlab.mixin.CustomDisplay
             matlab.mixin.CustomDisplay.displayPropertyGroups(self,propgroup);
         end
         function pg = getPropertyGroups(self)
+            % Cosmetic
             if ~isscalar(self)
                 pg = getPropertyGroups@matlab.mixin.CustomDisplay(self);
             else
                 pg(1) = matlab.mixin.util.PropertyGroup(self.Vector,'Vector');
             end
+            % TODO Array of vectors of the same type should have format:
+            % 1x2 Featurevector array with properties:
+            %     Vector(Mean,Variance,Skewness)
         end
     end
 end
