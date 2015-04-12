598f66bf5a44cc125d8cfa0d00be91f9587a16a5
Sebastian Olsson <sebelino@hotmail.com>
2015-04-12 22:11:42 +0200

    More experimenting.

--- testsvm.m
+++ testsvm.m
@@ -1,16 +1,18 @@
-%X = [1 1;2 1;1 2;4 4;4 5;4 6];
-%X = [rand(100,1),rand(100,1);rand(100,1)+2,rand(100,1)+2];
-quantity = 1000;
+quantity = 200;
 rands = [rand(quantity,1),2*pi*rand(quantity,1)];
 polarands = [rands(:,1),rands(:,1)].*[cos(rands(:,2)),sin(rands(:,2))];
-X = [ones(quantity,2)+polarands;3*ones(quantity,2)+polarands];
-%y = {'A','A','A','B','B','B'}';
+X = [ones(quantity,2)+polarands;2.1*ones(quantity,2)+polarands];
 y = [repmat({'A'},quantity,1);repmat({'B'},quantity,1)];
 
 m = fitcsvm(X,y);
 sv = m.SupportVectors;
+cv = crossval(m);
+classloss = kfoldLoss(cv);
 
 figure
-gscatter(X(:,1),X(:,2),y)
+whitebg(1,'k')
+gscatter(X(:,1),X(:,2),y,[],[],6*[1,1])
 hold on
-plot(sv(:,1),sv(:,2),'ko','MarkerSize',10)
+plot(sv(:,1),sv(:,2),'yo','MarkerSize',3)
+
+
