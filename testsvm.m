931fb0a1b112152835583d6ae643206551429922
Sebastian Olsson <sebelino@hotmail.com>
2015-04-12 18:29:42 +0200

    SVM expermient separating two circular clusters.

--- testsvm.m
+++ testsvm.m
@@ -1,5 +1,11 @@
-X = [1 1;2 1;1 2;4 4;4 5;4 6];
-y = {'A','A','A','B','B','B'}';
+%X = [1 1;2 1;1 2;4 4;4 5;4 6];
+%X = [rand(100,1),rand(100,1);rand(100,1)+2,rand(100,1)+2];
+quantity = 1000;
+rands = [rand(quantity,1),2*pi*rand(quantity,1)];
+polarands = [rands(:,1),rands(:,1)].*[cos(rands(:,2)),sin(rands(:,2))];
+X = [ones(quantity,2)+polarands;3*ones(quantity,2)+polarands];
+%y = {'A','A','A','B','B','B'}';
+y = [repmat({'A'},quantity,1);repmat({'B'},quantity,1)];
 
 m = fitcsvm(X,y);
 sv = m.SupportVectors;
