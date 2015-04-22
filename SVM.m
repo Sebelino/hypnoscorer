f88560991eff0cef54c8f70388cfc16baa9e7a8c
Sebastian Olsson <sebelino@hotmail.com>
2015-04-22 16:00:52 +0200

    Applied SVM classification to the mean/variance data, but the hyperplane looks wrong.

--- SVM.m
+++ SVM.m
@@ -2,13 +2,30 @@ classdef SVM < Classifier
     % Signal Support vector machine based classifier.
 
     properties(SetAccess='private')
-        model % The SVM classifier model
+        Model % The SVM classifier model
     end
     methods
-        function self = SVM()
+        function self = SVM(predictors,labels)
+            predictormatrix = predictors.matrix();
+            self.Model = fitcsvm(predictormatrix,labels);
         end
         function train(predictors,labels)
-            % Trains a SVM model
+             % Trains a SVM model
+        end
+        function plot(self)
+            sv = self.Model.SupportVectors;
+            % Plot vectors
+            plot(sv(:,1),sv(:,2),'yo','MarkerSize',5)
+            % Plot hyperplane
+            w = sum(repmat(self.Model.Alpha,1,2).*sv);
+            svcentroid = sum(sv)/size(sv,1);
+            bias = w*svcentroid'/w(2);
+            xlimits = xlim;
+            linex = linspace((3*xlimits(1)+xlimits(2))/4,(xlimits(1)+3*xlimits(2))/4);
+            %liney = -w(1)*linex/w(2)-m.Bias;
+            liney = -w(1)*linex/w(2)+bias;
+            plot(linex,liney)
+
         end
     end
 end
--- hypnoscorer.m
+++ hypnoscorer.m
@@ -31,10 +31,20 @@ labels = repmat(labels,1,segmentsperannotation)';
 labels = labels(:);
 %labels = labels(ismember(labels,'WR1234'));
 %labels(labels == 'M') = '4';
+bilabels = labels;
+bilabels(ismember(labels,['4','3','M'])) = 'D';
+bilabels(ismember(labels,['1','2','R','W'])) = 'S';
 
 %plot3D(efs,labels)
 %animate()
 
-%svm = SVM()
-%svm.train(efs,)
+indices = find(ismember(labels,['W','4']));
+labels = labels(indices);
+efs = efs(indices);
+
+plot2D(efs,labels)
+
+svm = SVM(efs,labels);
+hold on
+svm.plot()
 
