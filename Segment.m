910ef6008c74fafa5ddf3a07e351f50207e4b7a4
Sebastian Olsson <sebelino@hotmail.com>
2015-05-05 01:45:21 +0200

    Fixed and moved the computation of the Hjorth parameters.

--- Segment.m
+++ Segment.m
@@ -7,14 +7,28 @@ classdef Segment < Signal
         end
         % Extract a set of features
         function features=features(self)
+            [activity,mobility,complexity] = self.hjorthparameters;
             fs = struct( ...
                 'Mean',mean(self.quantity) ...
-               ,'Variance',var(self.quantity) ...
+               ,'Variance',activity ...
                ,'StandardDeviation',std(self.quantity) ...
                ,'Skewness',skewness(self.quantity) ...
                ,'Kurtosis',kurtosis(self.quantity) ...
+               ,'HjorthMobility',mobility ...
+               ,'HjorthComplexity',complexity ...
             );
             features = Featurevector(fs);
+        end
+        function [activity,mobility,complexity] = hjorthparameters(self)
+            x = self.time;
+            y = self.quantity;
+            y1 = diff(y)./diff(x);
+            x1 = cumsum(diff(x))-diff(x);
+            y2 = diff(y1)./diff(x1);
+            x2 = cumsum(diff(x1))-diff(x1);
+            activity = var(y);
+            mobility = sqrt(var(y1)/var(y));
+            complexity = sqrt(var(y)*var(y2)/(var(y)^2));
         end
     end
 end
