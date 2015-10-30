28313c898d477d00f8b4dbdfe60e7d5eec63cf7c
Sebastian Olsson <sebelino@hotmail.com>
2015-10-30 11:25:22 +0100

    Support for SVM multiclass classification (one-against-one).

--- SVM.m
+++ SVM.m
@@ -2,18 +2,35 @@ classdef SVM < Classifier
     % Signal Support vector machine based classifier.
 
     properties(SetAccess='private')
-        Model % The SVM classifier model
+        Model % Struct mapping two labels (string containing two characters and a leading 'L') to a SVM model
     end
     methods
         function self = SVM(labeledfeaturevectors)
             predictormatrix = labeledfeaturevectors.matrix();
             labels = [labeledfeaturevectors.Label]';
-            self.Model = fitcsvm(predictormatrix,labels,'KernelScale','auto');
+            classes = unique(labels);
+            self.Model = struct();
+            for i = 1:numel(classes)
+                label1 = classes(i);
+                for j = i+1:numel(classes)
+                    label2 = classes(j);
+                    key = ['L',label1,label2];
+                    indices = [find(labels==label1);find(labels==label2)];
+                    m = predictormatrix(indices,:);
+                    self.Model.(key) = fitcsvm(m,labels(indices),'KernelFunction',kernel,'KernelScale','auto');
+                end
+            end
         end
         function featureset = predict(self,predictors)
             % Predicts the labels for the given data
-            [labels,score] = predict(self.Model,predictors.matrix());
-            featureset = arrayfun(@(i){LabeledFeaturevector(predictors(i).Vector,labels(i))},(1:size(labels,1)));
+            alllabels = [];
+            for f = fieldnames(self.Model)'
+                [labels,score] = predict(self.Model.(f{:}),predictors.matrix());
+                alllabels = [alllabels,labels];
+            end
+            %[M,F,C] = mode(uint8(alllabels)'); %TODO
+            winnerlabels = mode(alllabels')';
+            featureset = arrayfun(@(i){LabeledFeaturevector(predictors(i).Vector,winnerlabels(i))},(1:size(winnerlabels,1)));
             featureset = [featureset{:}]';
         end
         function plot(self)
